7d29ed75578c93fb3ed28c95f95cfbb1518a6647
xyz <xiangyanga23@163.com>
2015-11-11 15:10:28 +0800

    first commit pro

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright (c) 2015年 com.xyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright (c) 2015年 com.xyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
